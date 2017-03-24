46535dd912c954ab6f021f0f97e0b4ee41669a18
dtk0528 <b03901039@ntu.edu.tw>
2017-03-25 07:29:23 +0800

    linted
    Former-commit-id: 451236d3035d86f02dd971d036f8cbfb87b4e644

--- server.js
+++ server.js
@@ -13,182 +13,191 @@ const assert = require('assert');
 const url = 'mongodb://test:test@ds060009.mlab.com:60009/eslab1_chat_room';
 
 const clients = [];
-var curMsgIdx = 0;
+let curMsgIdx = 0;
 
 http.listen(port, () => {
-    console.log(`listening on localhost:8080 and ${ip.address()}:8080`);
+  console.log(`listening on localhost:8080 and ${ip.address()}:8080`);
 });
 
 app.use(express.static(`${__dirname}/public`));
 
 function getUsersList() {
-    const usersList = [];
-    console.log("\n========== Current Users List ==========");
-    for (let i = 0; i < clients.length; i += 1) {
-        usersList[i] = clients[i].n;
-        console.log(clients[i].n);
-    }
-    console.log("====== End of Current Users List =======\n");
-    return usersList;
+  const usersList = [];
+  console.log('\n========== Current Users List ==========');
+  for (let i = 0; i < clients.length; i += 1) {
+    usersList[i] = clients[i].n;
+    console.log(clients[i].n);
+  }
+  console.log('====== End of Current Users List =======\n');
+  return usersList;
 }
 
 function setUserTyping(index) {
-    const usersList = [];
-    for (let i = 0; i < clients.length; i += 1) {
-        usersList[i] = clients[i].n;
-    }
-    usersList[index] = `💬 ${clients[index].n}`;
-    return usersList;
+  const usersList = [];
+  for (let i = 0; i < clients.length; i += 1) {
+    usersList[i] = clients[i].n;
+  }
+  usersList[index] = `💬 ${clients[index].n}`;
+  return usersList;
 }
 
 function insertDocuments(db, wtinsert, callback) {
-    // Get the documents collection
-    const userProfile = db.collection('userProfile');
-    // Insert some documents
-    userProfile.insert([wtinsert], (err, result) => {
-        assert.equal(err, null);
-        assert.equal(1, result.result.n);
-        assert.equal(1, result.ops.length);
-        callback(result);
-    });
+  // Get the documents collection
+  const userProfile = db.collection('userProfile');
+  // Insert some documents
+  userProfile.insert([wtinsert], (err, result) => {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    assert.equal(1, result.ops.length);
+    callback(result);
+  });
 }
 
 function addMesage(db, wtinsert, callback) {
-    // Get the documents collection
-    const messages = db.collection('messages');
-    // Insert some documents
-    messages.insert([wtinsert], (err, result) => {
-        assert.equal(err, null);
-        assert.equal(1, result.result.n);
-        assert.equal(1, result.ops.length);
-        callback(result);
-    });
+  // Get the documents collection
+  const messages = db.collection('messages');
+  // Insert some documents
+  messages.insert([wtinsert], (err, result) => {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    assert.equal(1, result.ops.length);
+    callback(result);
+  });
 }
 
 function findDocuments(db, wtfind, callback) {
-    // Get the documents collection
-    const userProfile = db.collection('userProfile');
-    // Find some documents
-    userProfile.find(wtfind).toArray((err, docs) => {
-        assert.equal(err, null);
-        callback(docs);
-    });
+  // Get the documents collection
+  const userProfile = db.collection('userProfile');
+  // Find some documents
+  userProfile.find(wtfind).toArray((err, docs) => {
+    assert.equal(err, null);
+    callback(docs);
+  });
 }
 
 io.on('connection', (socket) => {
-    let addedUser = false;
+  let addedUser = false;
 
-    socket.on('download message', () => {
-        console.log('message downloaded');
-        MongoClient.connect(url, (err, db) => {
-            const messages = db.collection('messages');
-            var cursor = messages.find({});
-            cursor.forEach( function(myDoc) { 
-                const tmp = {
-                    username: myDoc.username,
-                    message: myDoc.message,
-                    time: myDoc.time
-                };
-                socket.emit('chat message', (tmp));
-             } );
-            //db.users.find().forEach( function(myDoc) { print( "user: " + myDoc.name ); } );
+  socket.on('download message', () => {
+    console.log('message downloaded');
+    MongoClient.connect(url, (err, db) => {
+      const messages = db.collection('messages');
+      const cursor = messages.find({});
+      cursor.forEach((myDoc) => {
+        const tmp = {
+          username: myDoc.username,
+          message: myDoc.message,
+          time: myDoc.time,
+        };
+        socket.emit('chat message', (tmp));
+      });
+      // db.users.find().forEach( function(myDoc) { print( "user: " + myDoc.name ); } );
 
-            //console.log('Message added to server');
-            //db.dropDatabase();
-            db.close();
-        });
-        //socket.emit('chat message', msg);        
+      // console.log('Message added to server');
+      // db.dropDatabase();
+      db.close();
     });
-    
-    socket.on('send chat message', (msg) => {
-        socket.broadcast.emit('chat message', msg);
-        MongoClient.connect(url, (err, db) => {
-            assert.equal(null, err);
-            addMesage(db, { msgIdx: curMsgIdx,
-                            username: msg.username,
-                            message: msg.message,
-                            time: msg.time          }, () => {});
-            curMsgIdx++;
-            console.log('Message added to server');
-            //db.dropDatabase();
-            db.close();
-        });
+    // socket.emit('chat message', msg);
+  });
+
+  socket.on('send chat message', (msg) => {
+    socket.broadcast.emit('chat message', msg);
+    MongoClient.connect(url, (err, db) => {
+      assert.equal(null, err);
+      addMesage(db, {
+        msgIdx: curMsgIdx,
+        username: msg.username,
+        message: msg.message,
+        time: msg.time,
+      }, () => {});
+      curMsgIdx += 1;
+      console.log('Message added to server');
+      // db.dropDatabase();
+      db.close();
     });
+  });
 
-    socket.on('login', (user) => {
-        if (addedUser) return;
-        MongoClient.connect(url, (err, db) => {
-            assert.equal(null, err);
-            console.log('User ' + user.username + ' connected correctly to server');
-            findDocuments(db, { username: user.username }, (doc) => {
-                if (!doc[0] || doc[0].password !== user.password) {
-                    socket.emit('login entry', false);
-                } else {
-                    socket.emit('login entry', true);
-                    addedUser = true;
-                    socket.username = user.username;
-                    io.emit('info', `New user: ${user.username}`);
-                    clients.push(socket);
-                    clients[clients.indexOf(socket)].n = user.username;
-                    io.emit('update userlist', getUsersList());
-                    io.emit('user joined', {
-                        username: socket.username,
-                        numUsers: clients.length,
-                    });
-                }
-                db.close();
-            });
-        });
-    });
-
-    socket.on('register', (user) => {
-        if (addedUser) return;
-
-        MongoClient.connect(url, (err, db) => {
-            assert.equal(null, err);
-            console.log('Connected correctly to server');
-            findDocuments(db, { username: user.username }, (doc) => {
-                if (!doc[0]) {
-                    insertDocuments(db, { username: user.username, password: user.password }, () => {});
-                    socket.emit('register entry', true);
-                    addedUser = true;
-                    socket.username = user.username;
-                    io.emit('info', `New user: ${user.username}`);
-                    clients.push(socket);
-                    clients[clients.indexOf(socket)].n = user.username;
-                    io.emit('update userlist', getUsersList());
-                    io.emit('user joined', {
-                        username: socket.username,
-                        numUsers: clients.length,
-                    });
-                } else {
-                    socket.emit('register entry', false);
-                }
-                db.close();
-            });
-        });
-    });
-
-    socket.on('typing', () => {
-        io.emit('typing signal', setUserTyping(clients.indexOf(socket)));
-        socket.broadcast.emit('typing', {
+  socket.on('login', (user) => {
+    if (addedUser) return;
+    MongoClient.connect(url, (err, db) => {
+      assert.equal(null, err);
+      console.log(`User ${user.username} connected correctly to server`);
+      findDocuments(db, {
+        username: user.username,
+      }, (doc) => {
+        if (!doc[0] || doc[0].password !== user.password) {
+          socket.emit('login entry', false);
+        } else {
+          socket.emit('login entry', true);
+          addedUser = true;
+          socket.username = user.username;
+          io.emit('info', `New user: ${user.username}`);
+          clients.push(socket);
+          clients[clients.indexOf(socket)].n = user.username;
+          io.emit('update userlist', getUsersList());
+          io.emit('user joined', {
             username: socket.username,
-        });
-    });
-
-    socket.on('not typing', () => {
-        io.emit('typing signal', getUsersList());
-        socket.broadcast.emit('stop typing', {
-            username: socket.username,
-        });
-    });
-
-    socket.on('disconnect', () => {
-        if (addedUser) {
-            io.emit('info', `User ${clients[clients.indexOf(socket)].n} disconnected.`);
-            console.log(`User ${clients[clients.indexOf(socket)].n} disconnected.`);
-            clients.splice(clients.indexOf(socket), 1);
-            socket.broadcast.emit('update userlist', getUsersList());
+            numUsers: clients.length,
+          });
         }
+        db.close();
+      });
     });
+  });
+
+  socket.on('register', (user) => {
+    if (addedUser) return;
+
+    MongoClient.connect(url, (err, db) => {
+      assert.equal(null, err);
+      console.log('Connected correctly to server');
+      findDocuments(db, {
+        username: user.username,
+      }, (doc) => {
+        if (!doc[0]) {
+          insertDocuments(db, {
+            username: user.username,
+            password: user.password,
+          }, () => {});
+          socket.emit('register entry', true);
+          addedUser = true;
+          socket.username = user.username;
+          io.emit('info', `New user: ${user.username}`);
+          clients.push(socket);
+          clients[clients.indexOf(socket)].n = user.username;
+          io.emit('update userlist', getUsersList());
+          io.emit('user joined', {
+            username: socket.username,
+            numUsers: clients.length,
+          });
+        } else {
+          socket.emit('register entry', false);
+        }
+        db.close();
+      });
+    });
+  });
+
+  socket.on('typing', () => {
+    io.emit('typing signal', setUserTyping(clients.indexOf(socket)));
+    socket.broadcast.emit('typing', {
+      username: socket.username,
+    });
+  });
+
+  socket.on('not typing', () => {
+    io.emit('typing signal', getUsersList());
+    socket.broadcast.emit('stop typing', {
+      username: socket.username,
+    });
+  });
+
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      io.emit('info', `User ${clients[clients.indexOf(socket)].n} disconnected.`);
+      console.log(`User ${clients[clients.indexOf(socket)].n} disconnected.`);
+      clients.splice(clients.indexOf(socket), 1);
+      socket.broadcast.emit('update userlist', getUsersList());
+    }
+  });
 });
